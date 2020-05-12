df5b3f7a6f94bb2b4cbe52372d1fd12308405cee
dutenrapha <dutenrapha@hotmail.com>
2020-05-12 17:18:36 -0300

    Inicio converção p

--- printf/ft_print_p.c
+++ printf/ft_print_p.c
@@ -6,7 +6,7 @@
 /*   By: rdutenke <rdutenke@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/12 16:06:27 by rdutenke          #+#    #+#             */
-/*   Updated: 2020/05/12 16:25:32 by raphael          ###   ########.fr       */
+/*   Updated: 2020/05/12 17:01:29 by raphael          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void    ft_print_p(t_ptf *pms)
 {
     pms->v_int = va_arg(pms->ap, size_t);
-    pms->v_str = ft_strjoin("0x7ffc", ft_itoa_hex(pms->v_int, 'd'));
+    pms->v_str = ft_strjoin("0x", ft_itoa_hex(pms->v_int, 'd'));
     ft_putstr_fd(pms->v_str, 1);
     pms->len += ft_strlen(pms->v_str) - 1;
 }
