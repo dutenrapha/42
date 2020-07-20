276281ee212ded62a0cc6bcaf586d69b07d575cb
dutenrapha <dutenrapha@hotmail.com>
2020-07-20 09:01:05 -0300

    Correção bug de entradas impróprias

--- ft_printf/ft_flag_minus.c
+++ ft_printf/ft_flag_minus.c
@@ -6,7 +6,7 @@
 /*   By: rdutenke <rdutenke@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/29 15:50:48 by rdutenke          #+#    #+#             */
-/*   Updated: 2020/06/01 16:56:40 by rdutenke         ###   ########.fr       */
+/*   Updated: 2020/07/20 08:56:47 by rdutenke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,24 @@
 void ft_flag_minus(t_ptf *parms)
 {
 	int temp;
-
+	char *pos;
+	char *pre;
+	
 	temp = 0;
-	if (parms->flag[1] == '*')
+	pre = ft_strchr(parms->flag,'.');
+	pos = ft_strchr(parms->flag,'*');
+	if(pos != NULL)
 	{
-		temp = va_arg(parms->ap, int);
-		if (temp <0)
-			parms->width = -temp;
-		else
-		parms->width = temp;
+		if (*pos == '*' && pre == NULL)
+		{
+			temp = va_arg(parms->ap, void *);
+			if (temp <0)
+				parms->width = -temp;
+			else
+			parms->width = temp;
+		}
 	}
+	
 	if (parms->width == 0)
 		ft_get_width(parms, 1);
 	ft_check_precision(parms);
---
+++ ft_printf/main8.c
@@ -0,0 +1,27 @@
+#include "libftprintf.h"
+#include <stdio.h>
+int main()
+{
+int		a = 12;
+int             i = 8;
+int             j = -12;
+int     l = 0;
+
+
+
+
+// printf(" --- printf : %d\n", printf("%-00000-----*i, %---0.*d, %0-0-0-0-0.*d, %-0-0-0-0-.*d, %-----.*d", a, i, a, i, a, i, a, i, a, i));
+// ft_printf(" --- ft_printf : %d\n\n", ft_printf("%-00000-----*i, %---0.*d, %0-0-0-0-0.*d, %-0-0-0-0-.*d, %-----.*d", a, i, a, i, a, i, a, i, a, i));
+
+// printf(" --- printf : %d\n", printf("%-00000-----*i, %---0.*d, %0-0-0-0-0.*d, %-0-0-0-0-.*d, %-----.*d", a, j, a, j, a, j, a, j, a, j));
+// ft_printf(" --- ft_printf : %d\n\n", ft_printf("%-00000-----*i, %---0.*d, %0-0-0-0-0.*d, %-0-0-0-0-.*d, %-----.*d", a, j, a, j, a, j, a, j, a, j));
+
+printf(" --- printf : %d\n", printf("%-00000-----*i, %---0.*d, %0-0-0-0-0.*d, %-0-0-0-0-.*d, %-----.*d", a, l, a, l, a, l, a, l, a, l));
+ft_printf(" --- ft_printf : %d\n\n", ft_printf("%-00000-----*i, %---0.*d, %0-0-0-0-0.*d, %-0-0-0-0-.*d, %-----.*d", a, l, a, l, a, l, a, l, a, l));
+
+
+printf(" --- printf : %d\n", printf("%c, %-c, %12c, %-3c, %-1c, %1c, %-2c, %-4c, %5c, %3c, %-*c, %-*c, %*c, %*c", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0));
+ft_printf(" --- ft_printf : %d\n\n", ft_printf("%c, %-c, %12c, %-3c, %-1c, %1c, %-2c, %-4c, %5c, %3c, %-*c, %-*c, %*c, %*c", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0));
+
+return (0);
+}
