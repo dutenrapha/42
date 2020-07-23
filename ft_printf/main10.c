7beb60f056620acf1fc3a9da149ab8fed4ae4b10
dutenrapha <dutenrapha@hotmail.com>
2020-07-23 10:24:17 -0300

    Atualização main

---
+++ ft_printf/main10.c
@@ -0,0 +1,11 @@
+#include "libftprintf.h"
+#include <stdio.h>
+int main()
+{
+
+printf(" --- printf : %d\n", printf("%%"));
+ft_printf(" --- ft_printf : %d\n", ft_printf("%%"));
+printf("\n");
+
+return (0);
+}
