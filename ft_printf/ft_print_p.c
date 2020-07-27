0ad019d7591cf7a74510ef2aee07ce3038676418
dutenrapha <dutenrapha@hotmail.com>
2020-07-27 17:31:45 -0300

    Correção bug Conversão p

--- ft_printf/ft_print_p.c
+++ ft_printf/ft_print_p.c
@@ -6,7 +6,7 @@
 /*   By: rdutenke <rdutenke@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/12 16:06:27 by rdutenke          #+#    #+#             */
-/*   Updated: 2020/07/24 13:35:25 by rdutenke         ###   ########.fr       */
+/*   Updated: 2020/07/27 17:24:28 by rdutenke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,13 @@ void	ft_print_p(t_ptf *parms)
 	temp = NULL;
 	parms->v_p = va_arg(parms->ap, void *);
 	temp = ft_itoa_base((intptr_t)parms->v_p, HEX_BASE_L);
-	parms->v_str = ft_strjoin("0x", temp);
+	if (*temp != '0')
+		parms->v_str = ft_strjoin("0x", temp);
+	else
+	{
+		parms->v_str = ft_strjoin("0x", "");
+	}
+	
 	free(temp);
 	parms->len_c = ft_strlen(parms->v_str);
 	parms->len += ft_strlen(parms->v_str);
--- ft_printf/main10.c
+++ ft_printf/main10.c
@@ -3,8 +3,8 @@
 int main()
 {
 
-printf(" --- printf : %d\n", printf("%5%"));
-ft_printf(" --- ft_printf : %d\n", ft_printf("%5%"));
+printf(" --- printf : %d\n", printf("%.p", NULL));
+ft_printf(" --- ft_printf : %d\n", ft_printf("%.p", NULL));
 printf("\n");
 
 return (0);
