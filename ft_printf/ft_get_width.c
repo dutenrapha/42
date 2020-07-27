b2320d6e68decf2fde456b6024fa4864788a7f7c
dutenrapha <dutenrapha@hotmail.com>
2020-07-27 15:07:49 -0300

    correção bug ft_match

--- ft_printf/ft_get_width.c
+++ ft_printf/ft_get_width.c
@@ -6,24 +6,49 @@
 /*   By: rdutenke <rdutenke@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/29 15:49:07 by rdutenke          #+#    #+#             */
-/*   Updated: 2020/07/27 14:58:59 by rdutenke         ###   ########.fr       */
+/*   Updated: 2020/07/27 15:06:36 by rdutenke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static int	ft_len_flag(t_ptf *parms, int k)
+{
+	int i;
+	int j;
+	int end;
+	
+	end = 0;
+	j = 0;
+	while (1)
+	{
+		i = 0;
+		while (BREAKS[i] != '\0' && end == 0)
+		{
+			if (parms->flag[j + k] == BREAKS[i])
+				end = 1;
+			i++;
+		}
+		j++;
+		if (end == 1)
+			break;
+	}
+	return (j);
+}
+
 
 void	ft_get_width(t_ptf *parms, int k)
 {
-	size_t	l_flag;
+	int	l_flag;
 	char	*temp;
 
 	temp = NULL;
 	l_flag = 0;
-	while (!ft_match(parms->flag[l_flag + k], BREAKS))
-	{
-		l_flag++;
-	}
+	// while (!ft_match(parms->flag[l_flag + k], BREAKS))
+	// {
+	// 	l_flag++;
+	// }
+	l_flag = ft_len_flag(parms, k);
 	temp = ft_substr(parms->flag, k, l_flag);
 	parms->width = ft_atoi(temp);
 	free(temp);
