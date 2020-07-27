a1946d1ddbec1a631780d887289ee601cc83b9b2
dutenrapha <dutenrapha@hotmail.com>
2020-07-27 17:35:28 -0300

    Correção bug flag p

--- ft_printf/ft_print_p.c
+++ ft_printf/ft_print_p.c
@@ -6,7 +6,7 @@
 /*   By: rdutenke <rdutenke@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/12 16:06:27 by rdutenke          #+#    #+#             */
-/*   Updated: 2020/07/27 17:24:28 by rdutenke         ###   ########.fr       */
+/*   Updated: 2020/07/27 17:34:36 by rdutenke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ void	ft_print_p(t_ptf *parms)
 	temp = NULL;
 	parms->v_p = va_arg(parms->ap, void *);
 	temp = ft_itoa_base((intptr_t)parms->v_p, HEX_BASE_L);
-	if (*temp != '0')
-		parms->v_str = ft_strjoin("0x", temp);
+	if (*temp == '0' && parms->precision == 0)
+		parms->v_str = ft_strjoin("0x", "");
 	else
 	{
-		parms->v_str = ft_strjoin("0x", "");
+		parms->v_str = ft_strjoin("0x", temp);
 	}
 	
 	free(temp);
--- ft_printf/main10.c
+++ ft_printf/main10.c
@@ -7,5 +7,10 @@ printf(" --- printf : %d\n", printf("%.p", NULL));
 ft_printf(" --- ft_printf : %d\n", ft_printf("%.p", NULL));
 printf("\n");
 
+
+printf(" --- printf : %d\n", printf("%p", NULL));
+ft_printf(" --- ft_printf : %d\n", ft_printf("%p", NULL));
+printf("\n");
+
 return (0);
 }
